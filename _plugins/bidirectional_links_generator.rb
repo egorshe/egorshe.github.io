60e456da52e8c1b0b42c88fac68cf305c12079b5
egorshe <cynepmend@gmail.com>
2025-10-16 15:46:59 -0400

    test links

--- _plugins/bidirectional_links_generator.rb
+++ _plugins/bidirectional_links_generator.rb
@@ -1,41 +1,55 @@
 # frozen_string_literal: true
 
-# Generates bidirectional links and a graph between posts and projects.
-# Supports [[wiki-style]] and [[Title|alias]] links.
-# Produces an includes file `_includes/notes_graph.json`.
+# Generates bidirectional links and a site-wide graph between posts, projects, and pages.
+# Supports [[Wiki-style]] and [[Title|Alias]] links across all content.
+# Writes graph data to `_includes/notes_graph.json`.
 
 class BidirectionalLinksGenerator < Jekyll::Generator
   def generate(site)
     graph_nodes = []
     graph_edges = []
 
-    # Collect documents from posts and projects
-    posts = site.collections['posts']&.docs || []
+    # Collect all docs we want to include
+    posts    = site.collections['posts']&.docs || []
     projects = site.collections['projects']&.docs || []
-    all_docs = posts + projects + site.pages
+    pages    = site.pages.select { |p| p.data['title'] } # only pages with titles
 
+    all_docs = posts + projects + pages
     link_extension = site.config["use_html_extension"] ? '.html' : ''
 
-    # Convert [[wiki-links]] into HTML <a> tags
+    # --- Replace [[wiki-style links]] with HTML links ---
     all_docs.each do |current_doc|
-      all_docs.each do |potential_target|
-        # Get both filename-based and title-based regex patterns
+      all_docs.each do |target_doc|
+        next if target_doc == current_doc
+
+        # Build regexes for title and filename
         filename_pattern = Regexp.escape(
-          File.basename(potential_target.basename, File.extname(potential_target.basename))
+          File.basename(target_doc.basename, File.extname(target_doc.basename))
         ).gsub('\_', '[ _]').gsub('\-', '[ -]').capitalize
 
-        title_pattern = potential_target.data['title'] ? Regexp.escape(potential_target.data['title']) : nil
+        title_pattern = target_doc.data['title'] ? Regexp.escape(target_doc.data['title']) : nil
 
-        new_href = "#{site.baseurl}#{potential_target.url}#{link_extension}"
-        anchor_tag = "<a class='internal-link' href='#{new_href}'>\\1</a>"
+        new_href = "#{site.baseurl}#{target_doc.url}#{link_extension}"
 
         # [[Title|alias]]
-        current_doc.content.gsub!(/\[\[(#{filename_pattern}|#{title_pattern})\|(.+?)\]\]/i, "<a class='internal-link' href='#{new_href}'>\\2</a>") if title_pattern
+        if title_pattern
+          current_doc.content.gsub!(
+            /\[\[(#{filename_pattern}|#{title_pattern})\|(.+?)\]\]/i,
+            "<a class='internal-link' href='#{new_href}'>\\2</a>"
+          )
+        end
+
         # [[Title]]
-        current_doc.content.gsub!(/\[\[(#{filename_pattern}|#{title_pattern})\]\]/i, anchor_tag) if title_pattern
+        if title_pattern
+          anchor_tag = "<a class='internal-link' href='#{new_href}'>\\1</a>"
+          current_doc.content.gsub!(
+            /\[\[(#{filename_pattern}|#{title_pattern})\]\]/i,
+            anchor_tag
+          )
+        end
       end
 
-      # Grey out links that don’t match anything
+      # Mark unresolved links
       current_doc.content.gsub!(
         /\[\[([^\]]+)\]\]/i,
         <<~HTML.delete("\n")
@@ -48,7 +62,7 @@ class BidirectionalLinksGenerator < Jekyll::Generator
       )
     end
 
-    # Identify backlinks and create graph data
+    # --- Backlinks + Graph Generation ---
     all_docs.each do |current_doc|
       docs_linking_to_current = all_docs.select do |doc|
         doc.url != current_doc.url && doc.content.include?(current_doc.url)
@@ -56,24 +70,24 @@ class BidirectionalLinksGenerator < Jekyll::Generator
 
       # Graph node
       graph_nodes << {
-        id: doc_id_from_doc(current_doc),
+        id: doc_id(current_doc),
         path: "#{site.baseurl}#{current_doc.url}#{link_extension}",
         label: current_doc.data['title']
       }
 
-      # Store backlinks
+      # Add backlinks
       current_doc.data['backlinks'] = docs_linking_to_current
 
       # Graph edges
       docs_linking_to_current.each do |linked_doc|
         graph_edges << {
-          source: doc_id_from_doc(linked_doc),
-          target: doc_id_from_doc(current_doc)
+          source: doc_id(linked_doc),
+          target: doc_id(current_doc)
         }
       end
     end
 
-    # Write graph JSON
+    # Write graph data
     File.write('_includes/notes_graph.json', JSON.dump({
       edges: graph_edges,
       nodes: graph_nodes
@@ -82,7 +96,7 @@ class BidirectionalLinksGenerator < Jekyll::Generator
 
   private
 
-  def doc_id_from_doc(doc)
+  def doc_id(doc)
     doc.data['title'].to_s.bytes.join
   end
 end
